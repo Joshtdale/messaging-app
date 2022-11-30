4d76f2274a856813ed092ab540091746c7f744f8
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-30 17:07:21 +0000

    filter gc, text bubble overflow, timestamp

--- app/src/HomeNav.js
+++ app/src/HomeNav.js
@@ -8,15 +8,35 @@ function HomeNav() {
         <div className='row vw-100'>
             <div className="col text-center mt-1">
                 <button className='editButton btn'>Edit</button>
+                {/* <div className="dropdown mb-3">
+                    <div
+                        className="btn btn-secondary dropdown-toggle"
+                        role="button"
+                        id="dropdownMenuLink"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        Select Location
+                    </div>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <button className="dropdown-item">
+                            Main St
+                        </button>
+                        <button className="dropdown-item">
+                            S. Broadway
+                        </button>
+                    </div>
+                </div> */}
+                </div>
+                <div className="col text-center mt-2">
+                    Messages
+                </div>
+                <div className="col text-center createBtn">
+                    <img className='createBtn btn' src={createButton} alt='Create'></img>
+                </div>
             </div>
-            <div className="col text-center mt-2">
-                Messages
-            </div>
-            <div className="col text-center createBtn">
-                <img className='createBtn btn' src={createButton} alt='Create'></img>
-            </div>
-        </div>
-    )
+            )
 }
 
-export default HomeNav
+            export default HomeNav
