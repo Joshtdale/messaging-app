b2241760968c68ce89ee53be9dd3264a85dbd7fd
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-12-13 18:33:52 +0000

    adding firebase creds and packages

--- app/src/services/auth.constants.js
+++ app/src/services/auth.constants.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = process.env.REACT_APP_API_URL;git 
 export const LOGIN_ENDPOINT = "user/login/"
 export const REGISTER_ENDPOINT = "user/signup/"
 export const REFRESH_ENDPOINT = "token/refresh/"
